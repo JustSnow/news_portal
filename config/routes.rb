ddac05c0e7ff459b4b4ce2201b5fbcc511f76fe2
SnowMan <JustSnow17@yandex.ru>
2013-10-03 13:14:32 +0400

    main page, menu, style, delete files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 NewsPortal::Application.routes.draw do
+  devise_for :users
+
   mount RedactorRails::Engine => '/redactor_rails'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   resources :posts
-  root to: "posts#index"
+  root to: "welcome#index"
 end
