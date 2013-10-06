08b64f828ab03dbe549582b8bf973f8674cbbe36
JustSnow <JustSnow17@yandex.ru>
2013-10-06 18:55:27 +0400

    ajax delete post for admin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,11 +71,6 @@ class PostsController < ApplicationController
   # DELETE /posts/1.json
   def destroy
     @post.destroy
-
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
-    end
   end
 
   def feed
