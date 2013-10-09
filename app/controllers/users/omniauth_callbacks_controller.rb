3e46f308404412237dbd94c83d5bcc0dba0f70fb
SnowMan <JustSnow17@yandex.ru>
2013-10-09 11:39:41 +0400

    fix bug config devise, readme

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       sign_in_and_redirect @user, :event => :authentication
     else
-      flash[:notice] = "authentication error"
+      flash[:alert] = "authentication error"
       redirect_to root_path
     end
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -225,10 +225,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  OAUTH_CONFIG = YAML.load_file(Rails.root.join('config', 'oauth.yml'))
+  OAUTH_CONFIG = YAML.load_file(Rails.root.join('config', 'oauth.yml'))[::Rails.env]
+  config.omniauth :facebook, OAUTH_CONFIG['facebook']['app_id'], OAUTH_CONFIG['facebook']['app_secret']
   
-  config.omniauth :facebook, OAUTH_CONFIG['facebook']['app_id'], OAUTH_CONFIG['facebook']['app_secret'] if ENV['RAILS_ENV'] = "development"
-  config.omniauth :facebook, OAUTH_CONFIG['heroku_facebook']['app_id'], OAUTH_CONFIG['heroku_facebook']['app_secret'] if ENV['RAILS_ENV'] = "production"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
