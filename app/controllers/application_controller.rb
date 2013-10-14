e483f7e2225497f4604780118ff86b79ee2cebcb
SnowMan <JustSnow17@yandex.ru>
2013-10-14 14:08:16 +0400

    cashe category, delete application get_all_categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :all_categories
-
-  private
-  	def all_categories
-      @categories = Category.find(:all)
-    end
 end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -124,7 +124,7 @@ ActiveAdmin.setup do |config|
   # Active Admin resources and pages from here.
   #
   # config.before_filter :do_something_awesome
-  config.skip_before_filter :all_categories
+  # config.skip_before_filter :all_categories
 
 
   # == Register Stylesheets & Javascripts
